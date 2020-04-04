2e515c20949169e589567c422aca36c5dd794696
robert@klohr.com <39203582+RobertKlohr@users.noreply.github.com>
2020-04-03 18:29:33 -0700

    WIP

---
+++ src/PowerShellForLockpath/Private/Write-InteractiveHost.ps1
@@ -0,0 +1,55 @@
+﻿function Write-InteractiveHost {
+    <#
+    .SYNOPSIS
+        Forwards to Write-Host only if the host is interactive, else does nothing.
+
+    .DESCRIPTION
+        A proxy function around Write-Host that detects if the host is interactive
+        before calling Write-Host. Use this instead of Write-Host to avoid failures in
+        non-interactive hosts.
+
+        The Git repo for this module can be found here: http://aka.ms/PowerShellForGitHub
+
+    .EXAMPLE
+        Write-InteractiveHost "Test"
+        Write-InteractiveHost "Test" -NoNewline -f Yellow
+
+    .NOTES
+        Boilerplate is generated using these commands:
+        # $Metadata = New-Object System.Management.Automation.CommandMetaData (Get-Command Write-Host)
+        # [System.Management.Automation.ProxyCommand]::Create($Metadata) | Out-File temp
+#>
+
+    [CmdletBinding(
+        HelpUri = 'http://go.microsoft.com/fwlink/?LinkID=113426',
+        RemotingCapability = 'None')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification = "This provides a wrapper around Write-Host. In general, we'd like to use Write-Information, but it's not supported on PS 4.0 which we need to support.")]
+    param(
+        [Parameter(
+            Position = 0,
+            ValueFromPipeline,
+            ValueFromRemainingArguments)]
+        [System.Object] $Object,
+
+        [switch] $NoNewline,
+
+        [System.Object] $Separator,
+
+        [System.ConsoleColor] $ForegroundColor,
+
+        [System.ConsoleColor] $BackgroundColor
+    )
+
+    # Determine if the host is interactive
+    if ([Environment]::UserInteractive -and `
+            ![Bool]([Environment]::GetCommandLineArgs() -like '-noni*') -and `
+        (Get-Host).Name -ne 'Default Host') {
+        # Special handling for OutBuffer (generated for the proxy function)
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+
+        Write-Host @PSBoundParameters
+    }
+}
