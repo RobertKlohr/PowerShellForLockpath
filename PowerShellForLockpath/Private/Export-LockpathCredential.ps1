bf470c33068bea445fdf6d4f5e6e348447d06164
robert@klohr.com <39203582+RobertKlohr@users.noreply.github.com>
2020-12-09 19:43:13 -0800

    WIP

---
+++ PowerShellForLockpath/Private/Export-LockpathCredential.ps1
@@ -0,0 +1,61 @@
+﻿function Export-LockpathCredential {
+    <#
+    .SYNOPSIS
+        Attempts to export the API credential to the local file system.
+
+    .DESCRIPTION
+        Attempts to export the API credential to the local file system.
+
+        The Git repo for this module can be found here: https://github.com/RobertKlohr/PowerShellForLockpath
+
+    .PARAMETER Cookie
+        A .Net cookie object.
+
+    .PARAMETER Uri
+        Uri of the cookie.
+
+    .EXAMPLE
+        Export-LockpathCredential
+
+    .INPUTS
+        System.Net.Cookie
+
+    .OUTPUTS
+        None
+
+    .NOTES
+        Private helper method.
+
+    .LINK
+        https://github.com/RobertKlohr/PowerShellForLockpath/wiki
+    #>
+
+    [CmdletBinding(
+        ConfirmImpact = 'Low',
+        PositionalBinding = $false,
+        SupportsShouldProcess = $true)]
+
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Methods called within here make use of PSShouldProcess, and the switch is passed on to them inherently.')]
+
+    param(
+        [Parameter(
+            Mandatory = $true)]
+        [PSCredential]  $Credential
+    )
+
+    $level = 'Verbose'
+    $functionName = ($PSCmdlet.CommandRuntime.ToString())
+    $service = 'PrivateHelper'
+
+    Write-LockpathInvocationLog -Confirm:$false -WhatIf:$false -FunctionName $functionName -Level $level -Service $service
+
+    try {
+        Export-Clixml -InputObject $Credential -Path $Script:LockpathConfig.credentialFilePath -Depth 10 -Force
+        $message = 'success'
+    } catch {
+        $message = 'failed'
+        $level = 'Warning'
+    } finally {
+        Write-LockpathLog -Confirm:$false -WhatIf:$false -Message $message -FunctionName $functionName -Level $level -Service $service
+    }
+}
